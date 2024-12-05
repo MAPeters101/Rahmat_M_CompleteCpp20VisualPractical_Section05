796a570a2e6a96aeb20ac9f1b805f46bcb2c4680
Mark Peters <mapeters101@earthlink.net>
2024-12-05 16:31:06 -0500

    Complete lesson050.

--- lesson050.cpp
+++ lesson050.cpp
@@ -2,6 +2,7 @@
 
 using namespace std;
 
+/*
 int main() {
 	bool bTrue{ true };
 	bool bFalse{ false };
@@ -9,9 +10,24 @@ int main() {
 	cout << "True: " << bTrue << endl;
 	cout << "false: " << bFalse << endl;
 
+	if (bTrue) {
+		cout << "This will be printed because bTrue is true." << endl;
+	}
+	if (!bFalse) {
+		cout << "This will be printed because bFalse is false." << endl;
+	}
+
+
+	if (bTrue && !bFalse) {
+		cout << "This will be printed because bFalse is false and bTrue is true." << endl;
+	}
+	else {
+		cout << "This will be printed because bFalse is true and/or bTrue is false." << endl;
+
+	}
 	return 0;
 }
-
+*/
 
 
 
