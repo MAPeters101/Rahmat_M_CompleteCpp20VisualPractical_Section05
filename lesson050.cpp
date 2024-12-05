509e8d6c58a33aa25391e8f727c32ac9e2d0264e
Mark Peters <mapeters101@earthlink.net>
2024-12-05 16:24:16 -0500

    Work on lesson050 to 9:20.

--- lesson050.cpp
+++ lesson050.cpp
@@ -1,4 +1,16 @@
+#include <iostream>
 
+using namespace std;
+
+int main() {
+	bool bTrue{ true };
+	bool bFalse{ false };
+
+	cout << "True: " << bTrue << endl;
+	cout << "false: " << bFalse << endl;
+
+	return 0;
+}
 
 
 
