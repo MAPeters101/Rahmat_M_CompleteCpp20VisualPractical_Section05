ab97252e099533637e8ac92d490186ef3ad90ad6
Mark Peters <mapeters101@earthlink.net>
2024-12-10 15:24:35 -0500

    Complete lesson058.

--- lesson058.cpp
+++ lesson058.cpp
@@ -2,40 +2,43 @@
 
 using namespace std;
 
+/*
 int main() {
 	int Choice{ 0 };
 	bool OrderPlaced{ false };
 
-	cout << "Welcome to our online store!\n";
-	cout << "Menu:\n";
-	cout << "1. Order T-Shirt\n";
-	cout << "2. Order Jeans\n";
-	cout << "3. Order Shoes\n";
-	cout << "4. Checkout\n";
-	cout << "Enter your choice (1-4): ";
-	cin >> Choice;
 
-	switch (Choice) {
-	case 1:
-		cout << "T-Shirt added to your cart." << endl;
-		break;
-	case 2:
-		cout << "Jeans added to your cart." << endl;
-		break;
-	case 3:
-		cout << "Shoes added to your cart." << endl;
-		break;
-	case 4:
-		cout << "Thank you for your order!" << endl;
-		break;
-	default:
-		cout << "Invalide choice. Please select again." << endl;
-		break;
+	do {
+		cout << "Welcome to our online store!\n";
+		cout << "Menu:\n";
+		cout << "1. Order T-Shirt\n";
+		cout << "2. Order Jeans\n";
+		cout << "3. Order Shoes\n";
+		cout << "4. Checkout\n";
+		cout << "Enter your choice (1-4): ";
+		cin >> Choice;
 
-
-	}
+		switch (Choice) {
+		case 1:
+			cout << "T-Shirt added to your cart." << endl;
+			break;
+		case 2:
+			cout << "Jeans added to your cart." << endl;
+			break;
+		case 3:
+			cout << "Shoes added to your cart." << endl;
+			break;
+		case 4:
+			cout << "Thank you for your order!" << endl;
+			OrderPlaced = true;
+			break;
+		default:
+			cout << "Invalide choice. Please select again." << endl;
+			break;
+		}
+	} while (!OrderPlaced);
 
 	return 0;
 }
 
-
+*/
