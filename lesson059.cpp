a93862f67801f384dd27f7aa6aca377d46be3400
Mark Peters <mapeters101@earthlink.net>
2024-12-10 16:36:40 -0500

    Work on lesson059.

---
+++ lesson059.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+
+/*
+int main() {
+	int NumItems{ 0 };
+	double TotalCost{ 0.0 };
+
+	cout << "Number of items: ";
+	cin >> NumItems;
+
+	for (int i = 1; i <= NumItems; ++i) {
+		double ItemPrice{ 0 };
+		cout << "Enter the price of item " << (i + 1) << ": ";
+		cin >> ItemPrice;
+		TotalCost += ItemPrice;
+	}
+
+	cout << "Total cost: " << TotalCost << endl;
+
+	return 0;
+}
+
+*/
