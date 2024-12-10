c7635f98375ae91be1ed6dc6efff10a120f78f11
Mark Peters <mapeters101@earthlink.net>
2024-12-10 16:49:22 -0500

    Complete lesson060.

--- lesson060.cpp
+++ lesson060.cpp
@@ -2,6 +2,7 @@
 
 using namespace std;
 
+/*
 int main() {
 	string Text{ "Hey, how are you?" };
 	int VowelCount{ 0 };
@@ -12,7 +13,16 @@ int main() {
 	}
 	cout << UPPERCASE << endl;
 
+	for (char C : Text) {
+		char LowerCaseC = std::tolower(C);
+		if (LowerCaseC == 'a' || LowerCaseC == 'e' || LowerCaseC == 'i' || LowerCaseC == 'o' || LowerCaseC == 'u') {
+			VowelCount++;
+		}
+	}
+	cout << "Text: " << Text << endl;
+	cout << "Number of vowels: " << VowelCount << endl;
+
 	return 0;
 }
 
-
+*/
