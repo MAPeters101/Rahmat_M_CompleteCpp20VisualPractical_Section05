df570c3a1223976cba16dad2b956a38159910b50
Mark Peters <mapeters101@earthlink.net>
2024-12-10 14:17:02 -0500

    Work on lesson055.

---
+++ lesson055.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+int main() {
+	//int Choice;
+	//cout << "Menu: " << endl;
+	//cout << "1. Print Hello" << endl;
+	//cout << "2. Print World" << endl;
+	//cout << "3. Print Goodbye" << endl;
+	//cout << "Enter your choice (1-3): ";
+	//cin >> Choice;
+
+	//switch (Choice) {
+	//case 1:
+	//	cout << "Hello ";
+	//	break;
+	//case 2:
+	//	cout << "World";
+	//	break;
+	//case 3:
+	//	cout << "Goodbye!";
+	//	break;
+	//default:
+	//	cout << "Invalid choice!";
+
+	char Choice;
+	cout << "Menu: " << endl;
+	cout << "a. Print Hello" << endl;
+	cout << "b. Print World" << endl;
+	cout << "c. Print Goodbye" << endl;
+	cout << "Enter your choice (a-c): ";
+	cin >> Choice;
+
+	switch (Choice) {
+	case 'a':
+		cout << "Hello ";
+		[[fallthrough]];
+	case 'b':
+		cout << "World";
+		break;
+	case 'c':
+		cout << "Goodbye!";
+		break;
+	default:
+		cout << "Invalid choice!";
+
+
+	}
+
+	return 0;
+}
+*/
+
