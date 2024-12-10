51503f2a9a9fcff429b2d072d3af826ae1a07027
Mark Peters <mapeters101@earthlink.net>
2024-12-10 12:42:05 -0500

    Work on lesson052.

---
+++ lesson052.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+int main() {
+	int Number{ 0 };
+	cout << "Enter a positive number: ";
+	cin >> Number;
+
+	if (Number > 0) {
+		cout << "The number is positive." << endl;
+	}
+	else {
+		cout << "Wrong number!" << endl;
+	}
+
+	return 0;
+}
+
+*/
