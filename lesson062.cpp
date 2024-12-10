3a5c8787a743f8eadb6614f2724e65c2da7b1370
Mark Peters <mapeters101@earthlink.net>
2024-12-10 17:41:19 -0500

    Work on lesson062 to 8:10.

--- lesson062.cpp
+++ lesson062.cpp
@@ -1,11 +1,26 @@
 #include <iostream>
+#include <chrono>
+#include <ctime>
+#include <thread>
 
 using namespace std;
 
 int main() {
+	//while (true) {
+	//	cout << "This is an infinate loop.  Press Ctrl+c to stop." << endl;
+	//}
+
 	while (true) {
-		cout << "This is an infinate loop.  Press Ctrl+c to stop." << endl;
+		auto Now = std::chrono::system_clock::now();
+		std::time_t CurrentTime;
+		std::time(&CurrentTime);
+
+		char Buffer[26];
+
+		ctime_s(Buffer, sizeof(Buffer), &CurrentTime);
+		cout << "Current Time: " << Buffer << endl;
 	}
+
 
 	return 0;
 }
