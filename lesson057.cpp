77845ec1c3d2bcd33082f313dd3c67757577d504
Mark Peters <mapeters101@earthlink.net>
2024-12-10 14:59:30 -0500

    Complete lesson057.

---
+++ lesson057.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+int main() {
+	int Count = 0;
+
+	while (Count < 5) {
+		cout << "Count: " << Count << endl;
+		Count++;
+	}
+	cout << "Loop finished." << endl << endl;
+
+	int Choice{ 0 };
+	bool bCorrect{ false };
+
+	while (!bCorrect) {
+		cout << "Menu: " << endl;
+		cout << "1. Print Hello" << endl;
+		cout << "2. Print World" << endl;
+		cout << "3. Print Goodbye" << endl;
+		cout << "Enter your choice (1-3): ";
+		cin >> Choice;
+
+		switch (Choice) {
+		case 1:
+			cout << "Hello ";
+			bCorrect = true;
+			break;
+		case 2:
+			cout << "World";
+			bCorrect = true;
+			break;
+		case 3:
+			cout << "Goodbye!";
+			bCorrect = true;
+			break;
+		default:
+			cout << "Invalid choice!" << endl << endl;
+		}
+
+	}
+
+	return 0;
+}
+
+*/
