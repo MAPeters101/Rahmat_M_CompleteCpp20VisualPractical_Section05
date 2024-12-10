27ad3f9781a96d5ea923dd189d65b4a91fe44c8f
Mark Peters <mapeters101@earthlink.net>
2024-12-10 13:16:10 -0500

    Work on lesson054 to 6:40.

---
+++ lesson054.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int Age;
+	char HasDrivingTest;
+
+	cout << "Enter your age: ";
+	cin >> Age;
+
+	cout << "Have you passed the driving test? (Y/N): ";
+	cin >> HasDrivingTest;
+
+	if (Age >= 18) {
+		if (HasDrivingTest == 'y' || HasDrivingTest == 'Y') {
+			cout << "Congratulations!  You are eligible for a driving license." << endl;
+		}
+		else if (HasDrivingTest == 'n' || HasDrivingTest == 'N') {
+			cout << "You need to pass the driving test first." << endl;
+		}
+		else {
+			cout << "Invalid input for driving test status." << endl;
+		}
+	}
+	else {
+		cout << "You are not eligible for a driving licence." << endl;
+	}
+
+	return 0;
+}
+
+
