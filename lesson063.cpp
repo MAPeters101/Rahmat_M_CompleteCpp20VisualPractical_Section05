899d6a13b32545266ce1f5e1b52cbfcef3ef2cc4
Mark Peters <mapeters101@earthlink.net>
2024-12-11 12:40:02 -0500

    Process deposit for lesson063.

--- lesson063.cpp
+++ lesson063.cpp
@@ -2,7 +2,25 @@
 
 using namespace std;
 
-char DisplayMenu(int Balance) {
+char DisplayMenu(double Balance);
+void ProcessChoice(char Choice, double Balance);
+double Deposit(double Balance);
+
+
+int main() {
+	double Balance{ 1000.0 };
+	char Choice{ ' ' };
+
+	while (true) {
+		Choice = DisplayMenu(Balance);
+		ProcessChoice(Choice, Balance);
+		if (Choice == '4')
+			break;
+	}
+	return 0;
+}
+
+char DisplayMenu(double Balance) {
 	char Choice{ ' ' };
 	cout << "Bank Account Management System" << endl;
 	cout << "-------------------------------" << endl;
@@ -18,10 +36,11 @@ char DisplayMenu(int Balance) {
 	return Choice;
 }
 
-void ProcessChoice(char Choice) {
+void ProcessChoice(char Choice, double Balance) {
 	switch (Choice) {
 	case '1':
 		cout << "Processing Deposit.\n" << endl;
+		Balance = Deposit(Balance);
 		break;
 	case '2':
 		cout << "Processing Withdraw.\n" << endl;
@@ -37,15 +56,18 @@ void ProcessChoice(char Choice) {
 	}
 }
 
-int main() {
-	double Balance{ 1000.0 };
-	char Choice{ ' ' };
-
-	while (true) {
-		Choice = DisplayMenu(Balance);
-		ProcessChoice(Choice);
-		if (Choice == '4')
-			break;
+double Deposit(double Balance) {
+	double Deposit{ 0.0 };
+	cout << "Please enter deposit amount: ";
+	cin >> Deposit;
+	if (Deposit > 0) {
+		Balance += Deposit;
+		cout << "Deposit successful." << endl;
+		cout << "New balance: $" << Balance << endl << endl;
 	}
-	return 0;
+	else {
+		cout << "Invalid deposit amount.\n" << endl;
+	}
+	return Balance;
 }
+
